8d5755d266414e381fd4e273c22c5dbdc5b277cb
Pavan <112955317+ehpavan@users.noreply.github.com>
2025-10-17 22:58:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-const blogList = document.getElementById("blog-list");
-const blogContent = document.getElementById("blog-content");
-
+// Fetch and render blog list
 const blogs = [
-  { title: "Breaking Into Web Security", file: "blogs/first_blog.md" },
-  { title: "How I Build Tools That Think Like Hackers", file: "blogs/tools_blog.md" },
+  { title: "My First Blog", file: "first-blog.md" },
+  { title: "Second Blog", file: "second-blog.md" },
 ];
 
-blogs.forEach((blog) => {
-  const div = document.createElement("div");
-  div.className = "blog-item";
-  div.textContent = blog.title;
-  div.onclick = () => loadBlog(blog.file);
-  blogList.appendChild(div);
-});
+const blogList = document.getElementById("blog-list");
+const blogContainer = document.getElementById("blog-container");
 
-function loadBlog(file) {
-  fetch(file)
-    .then((res) => res.text())
-    .then((text) => {
-      blogContent.innerHTML = marked.parse(text);
-      blogContent.style.display = "block";
-      window.scrollTo({ top: blogContent.offsetTop - 80, behavior: "smooth" });
-    });
+function renderBlogList() {
+  blogs.forEach((blog) => {
+    const div = document.createElement("div");
+    div.className = "blog-item";
+    div.textContent = blog.title;
+    div.addEventListener("click", () => loadBlog(blog.file));
+    blogList.appendChild(div);
+  });
 }
+
+async function loadBlog(file) {
+  const response = await fetch(`./blogs/${file}`);
+  const text = await response.text();
+  blogContainer.innerHTML = marked.parse(text);
+  window.scrollTo({ top: blogContainer.offsetTop - 100, behavior: "smooth" });
+}
+
+renderBlogList();
