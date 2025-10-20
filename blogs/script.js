b2046da83cb22d2392f04592fbaa1eed5a6727cc
Pavan <112955317+ehpavan@users.noreply.github.com>
2025-10-20 11:45:34 +0530

    Update script.js

--- blogs/script.js
+++ blogs/script.js
@@ -3,6 +3,8 @@ const blogs = [
   { title: "Building Secure APIs", file: "first-blog.md", date: "2025-10-17" },
   { title: "Web Recon 101", file: "second-blog.md", date: "2025-10-18" },
   { title: "Advanced SQL Injection Notes", file: "third-blog.md", date: "2025-10-19" }
+    { title: "Hacking the Internet, The Untold Story Of a Hero", file: "fourth-blog.md", date: "2025-10-20" }
+
 ];
 
 const blogList = document.getElementById("blog-list");
@@ -20,7 +22,7 @@ blogs.forEach(async (b) => {
     const minutes = estimateReadingTime(content);
 
     const dateObj = new Date(b.date);
-    const formattedDate = dateObj.toLocaleDateString("en-US",{year:"numeric",month:"short",day:"numeric"});
+    const formattedDate = dateObj.toLocaleDateString("en-US", { year: "numeric", month: "short", day: "numeric" });
 
     const card = document.createElement("div");
     card.className = "blog-card";
@@ -28,7 +30,7 @@ blogs.forEach(async (b) => {
       <a href="blog.html?file=${b.file}" target="_blank">
         <h2>${b.title}</h2>
       </a>
-      <p class="meta">📅 ${formattedDate} &nbsp; ⏱️ ${minutes} min read</p>
+      <p class="meta">${formattedDate} · ${minutes} min read</p>
     `;
     blogList.appendChild(card);
   } catch(err) {
